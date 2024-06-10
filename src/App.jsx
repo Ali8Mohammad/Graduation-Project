5e502ec0fb17ea5ab123c225a63f58c0d5ebe661
unknown <alim94258@gmail.com>
2024-06-10 10:56:04 +0300

    github-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 // import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
-import {FontAwesomeIcon} from ' @fortawesome/react-fontawesome';
-
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function Home() {
   return <h2>Home</h2>;
 }
