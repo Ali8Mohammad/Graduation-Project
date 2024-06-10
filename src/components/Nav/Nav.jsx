370d50800319d177bac17db9fe8f05735252bc16
Noura-H-Mahmoud <nouramahmoud@Nouras-MacBook-Air.local>
2024-06-10 16:57:30 +0400

    props nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import './Nav.css';
-export default function Nav() {
+
+export default function Nav({ buttons }) {
   return (
     <nav className='NM_NavBar'>
         <div className='NM_Start'>
             <a className='NM_FindFlight' href="#" target="_blank" rel="noopener noreferrer">
-            <img  src="./src/assets/images/plane.svg" alt="plane icon" /> Find Flight
+            <img src="./src/assets/images/plane.svg" alt="plane icon" /> Find Flight
             </a>
-            <a  className='NM_FindStays' href="#" target="_blank" rel="noopener noreferrer">
+            <a className='NM_FindStays' href="#" target="_blank" rel="noopener noreferrer">
             <img src="./src/assets/images/bed.svg" alt="bed icon" /> Find Stays
             </a>
         </div>
         <img className='NM_Logo' src="./src/assets/images/Logo.svg" alt="logo" />
         <div className='NM_Buttons'>
-            <button className='NM_Login'>Login</button>
-            <button className='NM_Signup'>Sign up</button>
+            {buttons}
         </div>
-
     </nav>
-  )
+  );
 }
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+ import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
     createBrowserRouter,
     RouterProvider,
-  } from "react-router-dom";
+} from "react-router-dom";
 import './index.css';
 import LandingPage from './pages/LandingPage/LandingPage.jsx';
 
+const title = 'LIVE & TRAVEL';
+const para = 'Special offers to suit your plan';
+const navButtons = (
+    <>
+        <button className='NM_Login'>Login</button>
+        <button className='NM_Signup'>Sign up</button>
+    </>
+);
 
-  const title='LIVE & TRAVEL';
-  const para='Special offers to suit your plan';
-  const router = createBrowserRouter([
+const router = createBrowserRouter([
     {
-      path: "/",
-      element: <App />,
-      children:[
-        {
         path: "/home",
-        element: <LandingPage 
-          title={title} para={para}/>,
-        }
-      ]
+        element: <App />,
+        children: [
+            {
+                path: "/home",
+                element: <LandingPage title={title} para={para} navButtons={navButtons} />,
+            }
+        ]
     },
-  ]);
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-       <RouterProvider router={router} />
+        <RouterProvider router={router} />
     </React.StrictMode>
-) 
+);
+
