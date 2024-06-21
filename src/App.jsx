b41655f5fba3c335183901f029c6f2b7d681bb83
Ali8Mohammad <alim94258@gmail.com>
2024-06-21 14:27:08 +0300

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import FlightBooking from './Pages/Flight Flow/FlightBooking/FlightBooking'
 
 function App() {
   return (
     <>
-    <Outlet />
+    <FlightBooking />
     </>
   )
 }
