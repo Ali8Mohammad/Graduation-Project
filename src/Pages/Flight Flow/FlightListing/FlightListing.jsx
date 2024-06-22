fdec6ae611d910c51887b06d02548527b8d3090e
Ali8Mohammad <alim94258@gmail.com>
2024-06-22 18:16:55 +0300

    whitebg-navbar

--- src/Pages/Flight Flow/FlightListing/FlightListing.jsx
+++ src/Pages/Flight Flow/FlightListing/FlightListing.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import Navigation from '../../../Components/Nav/Nav';
-import Footer from '../../../Components/Footer/Footer';
-
-export default function FlightListing() {
+import React from 'react'
+import Navigation from '../../../Components/Nav/Nav'
+import Footer from '../../../Components/Footer/Footer'
+export default function FlightListing({navButtons, navButtonsForMobile}) {
   return (
     <>
-      <Navigation page="flightPage" />
-      <Footer />
+    <Navigation  buttons={navButtons} navMobile ={navButtonsForMobile} isLandingPage={false}/>
+    <Footer/>
     </>
-  );
+  )
 }
