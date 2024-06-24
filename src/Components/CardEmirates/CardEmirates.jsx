d669a751821f310462bb56f050340a819a57aa30
Rafif <hajouzeengrafif@gmail.com>
2024-06-25 01:57:21 +0300

    flightDeatils

--- src/Components/CardEmirates/CardEmirates.jsx
+++ src/Components/CardEmirates/CardEmirates.jsx
@@ -1,21 +1,20 @@
 // import { Row, Col } from 'react-bootstrap' 
-import './CardFlightDetailFinal.css'
+import './CardEmirates.css'
 import Card from 'react-bootstrap/Card';
 import './../../../src/index.css'
-const CardFlightDetailFinal = () => {
+const CardEmirates = () => {
     return (
-    <section className='Aya_Flight_Details_Final'>
-         <Card style={{ }} className='cards'>
+    <section className='Aya_Flight_Details_Final' style={{ display: 'flex', justifyContent: 'center',margin:'40px auto',width:'100%'}}>
+         <Card style={{ width: '1232px' }} className='cards'>
       <Card.Body>
         <div className='part1 d-flex justify-content-between'>
             <Card.Title className='title'>Emirates A380 Airbus</Card.Title>
-            <span className='span'>240 $</span>
         </div>
         <div className='part2 d-flex justify-content-between'>
             <Card.Title className='title'>Return Wed, Dec 8</Card.Title>
             <span className='span'>2h 28m</span>
         </div>
-        <div className='part3 d-flex flex-wrap'>
+        <div className='part3 d-flex ' style={{  width: '100%'}}>
             <div className="part3-1 d-flex">
                 <Card.Img className='image' src="./../src/assets/images/Emirates-line.png" />
                 <p className='p'>
@@ -23,7 +22,7 @@ const CardFlightDetailFinal = () => {
                     <span className='span2'>Airbus A320</span>
                 </p>
             </div>
-            <div className="part3-2 d-flex flex-wrap ">
+            <div className="part3-2" style={{  marginRight:'-24px'  }}>
                 <Card.Img className='image-airplane' src="./../src/assets/images/airplane.svg" />   
                 <Card.Img style={{color: "#D7E2EE" , height: "40px"}} className='line2' src="./../src/assets/images/Line2.svg" />   
                 <Card.Img className='image-Wifi' src="./../src/assets/images/Wifi.svg" />   
@@ -35,7 +34,7 @@ const CardFlightDetailFinal = () => {
                 <Card.Img className='image-round-airline' src="./../src/assets/images/ic_round-airline-seat-recline-normal.svg" />  
             </div>
         </div>
-        <div className="part4">
+        <div className="part4" style={{  display: 'flex',  margin: '40px auto',}}>
         <Card.Title className='span1'>12:00 pm</Card.Title>
         <Card.Title className='span2'>Newark(EWR)</Card.Title>
         <Card.Img className='image-line5' src="./../src/assets/images/Line5.svg" />   
@@ -50,4 +49,4 @@ const CardFlightDetailFinal = () => {
     )
 }
 
-export default CardFlightDetailFinal
+export default CardEmirates
